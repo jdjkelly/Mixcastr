95459f69436f985d4fa91fda7f8cb8ce050b8c49
Joshua Kelly <jdjkelly@gmail.com>
2011-10-01 19:30:18 -0400

    added files

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'soundcloud'
+
+get '/' do
+	# register a client with YOUR_CLIENT_ID as client_id_
+	# client = Soundcloud.new(:client_id => '3c3aecf09c9a6e46e721f6118612ca59')
+	# get 10 hottest tracks
+	# @tracks = client.get('/users/28428/tracks')
+	erb :index
+end
+
+get '/less' do
+	erb :less
+end
